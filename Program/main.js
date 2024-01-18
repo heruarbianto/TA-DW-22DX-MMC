940aad4eeb52c47b89cff9545c11f4e287b712de
ZaniEko <zani_eko_fitriyanto@teknokrat.ac.id>
2024-01-18 23:00:30 +0700

    ZANI : PENAMBAHAN ASSET IMAGE UNTUK MODAL BUTTON JELAHI DESTINASI

--- Program/main.js
+++ Program/main.js
@@ -15,26 +15,28 @@ menu.onclick = () => {
   layanan.style.display = "none";
 };
 
+  
   // pelajari lebih lanjut home di klik
   document.querySelector("#read-Home").addEventListener("click", () => {
-  // tampilkan modal
-  document.querySelector(".modal_layout").style.display = "flex";
-  // tampilkan image 1
-  document.querySelector("#modal_image").src = "img/a.jpg";
-  document.querySelector("#modal_image").alt = "gambar seputar teknologi hijau";
-  // tampilkan text paragraf
-  document.querySelector("#modal_text").textContent =
-    "Selamat datang di Teknologi Hijau, tempat di mana inovasi bertemu keberlanjutan. Kami adalah pionir dalam membawa solusi terkini untuk mewujudkan masa depan yang berkelanjutan. Dengan fokus pada energi terbarukan, transportasi ramah lingkungan, dan pengolahan limbah, kami berkomitmen untuk mengurangi dampak lingkungan dan menciptakan dunia yang lebih baik.";
-});
+    // tampilkan modal
+    document.querySelector(".modal_layout").style.display = "flex";
+    // tampilkan image 1
+    document.querySelector("#modal_image").src = "img/a.jpg";
+    document.querySelector("#modal_image").alt = "gambar seputar teknologi hijau";
+    // tampilkan text paragraf
+    document.querySelector("#modal_text").textContent =
+      "Selamat datang di Teknologi Hijau, tempat di mana inovasi bertemu keberlanjutan. Kami adalah pionir dalam membawa solusi terkini untuk mewujudkan masa depan yang berkelanjutan. Dengan fokus pada energi terbarukan, transportasi ramah lingkungan, dan pengolahan limbah, kami berkomitmen untuk mengurangi dampak lingkungan dan menciptakan dunia yang lebih baik.";
+  });
+  
+  document.querySelector("#modal_close_icon").addEventListener("click", () => {
+    // tutup modal
+    document.querySelector(".modal_layout").style.display = "none";
+  });
+  
+  // tutup modal di sembarang area
+  window.addEventListener("click", (e) => {
+  if (e.target == document.querySelector(".modal_layout")) {
+    document.querySelector(".modal_layout").style.display = "none";
+  }
+  });
 
-document.querySelector("#modal_close_icon").addEventListener("click", () => {
-  // tutup modal
-  document.querySelector(".modal_layout").style.display = "none";
-});
-
-// tutup modal di sembarang area
-window.addEventListener("click", (e) => {
-if (e.target == document.querySelector(".modal_layout")) {
-  document.querySelector(".modal_layout").style.display = "none";
-}
-});
