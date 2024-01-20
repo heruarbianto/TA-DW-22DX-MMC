c5c1429e38b39248c63363b9fd9aa6e5dfe17407
mediamahasiswacreative@gmail.com <heru_arbianto@teknokrat.ac.id>
2024-01-20 23:49:47 +0700

    Master : set tag a di navbar agar auto scroll ke id yang ditentukan

--- Program/main.js
+++ Program/main.js
@@ -15,59 +15,68 @@ menu.onclick = () => {
   layanan.style.display = "none";
 };
 
-  
-  // pelajari lebih lanjut home di klik
-  document.querySelector("#read-Home").addEventListener("click", () => {
-    // tampilkan modal
-    document.querySelector(".modal_layout").style.display = "flex";
-    // tampilkan image 1
-    document.querySelector("#modal_image").src = "img/a.jpg";
-    document.querySelector("#modal_image").alt = "gambar seputar teknologi hijau";
-    // tampilkan text paragraf
-    document.querySelector("#modal_text").textContent =
-      "Selamat datang di Teknologi Hijau, tempat di mana inovasi bertemu keberlanjutan. Kami adalah pionir dalam membawa solusi terkini untuk mewujudkan masa depan yang berkelanjutan. Dengan fokus pada energi terbarukan, transportasi ramah lingkungan, dan pengolahan limbah, kami berkomitmen untuk mengurangi dampak lingkungan dan menciptakan dunia yang lebih baik.";
-  });
-  
-  document.querySelector("#modal_close_icon").addEventListener("click", () => {
-    // tutup modal
-    document.querySelector(".modal_layout").style.display = "none";
-  });
-  
-  // tutup modal di sembarang area
-  window.addEventListener("click", (e) => {
+// pelajari lebih lanjut home di klik
+document.querySelector("#read-Home").addEventListener("click", () => {
+  // tampilkan modal
+  document.querySelector(".modal_layout").style.display = "flex";
+  // tampilkan image 1
+  document.querySelector("#modal_image").src = "img/a.jpg";
+  document.querySelector("#modal_image").alt = "gambar seputar teknologi hijau";
+  // tampilkan text paragraf
+  document.querySelector("#modal_text").textContent =
+    "Selamat datang di Teknologi Hijau, tempat di mana inovasi bertemu keberlanjutan. Kami adalah pionir dalam membawa solusi terkini untuk mewujudkan masa depan yang berkelanjutan. Dengan fokus pada energi terbarukan, transportasi ramah lingkungan, dan pengolahan limbah, kami berkomitmen untuk mengurangi dampak lingkungan dan menciptakan dunia yang lebih baik.";
+});
+
+document.querySelector("#modal_close_icon").addEventListener("click", () => {
+  // tutup modal
+  document.querySelector(".modal_layout").style.display = "none";
+});
+
+// tutup modal di sembarang area
+window.addEventListener("click", (e) => {
   if (e.target == document.querySelector(".modal_layout")) {
     document.querySelector(".modal_layout").style.display = "none";
   }
-  });
+});
 
-
-  // ketika button jelajahi destinasi di klik
-  document.querySelector("#Jelahai-Destinasi").addEventListener("click", () => {
+// ketika button jelajahi destinasi di klik
+document.querySelector("#Jelahai-Destinasi").addEventListener("click", () => {
   // menampilkan modal
   document.querySelector(".modal_layout").style.display = "flex";
   // menampilkan images b
   document.querySelector("#modal_image").src = "img/b.jpg";
-  document.querySelector("#modal_image").alt = "gambar jelajahi destinasi wisata";
+  document.querySelector("#modal_image").alt =
+    "gambar jelajahi destinasi wisata";
   // menampilkan text paragraf
   document.querySelector("#modal_text").textContent =
-   "Indonesia menawarkan destinasi ekowisata menakjubkan, seperti Pulau Menjangan di Bali dan Tanjung Ringgit di Lombok yang didedikasikan 90% untuk ekowisata. Taman Nasional Alas Purwo di Banyuwangi dan Taman Nasional Way Kambas di Lampung fokus pada pelestarian alam. Taman Nasional Tanjung Puting di Kalimantan Tengah menyuguhkan pengalaman unik melalui perjalanan sungai untuk melihat orangutan dilindungi.";
+    "Indonesia menawarkan destinasi ekowisata menakjubkan, seperti Pulau Menjangan di Bali dan Tanjung Ringgit di Lombok yang didedikasikan 90% untuk ekowisata. Taman Nasional Alas Purwo di Banyuwangi dan Taman Nasional Way Kambas di Lampung fokus pada pelestarian alam. Taman Nasional Tanjung Puting di Kalimantan Tengah menyuguhkan pengalaman unik melalui perjalanan sungai untuk melihat orangutan dilindungi.";
+});
+
+//menambahkan fitur auto scroll
+document.querySelector("#EnergiTerbarukan").addEventListener("click", () => {
+  const tentangKamiSection = document.getElementById("PanelSurya");
+  tentangKamiSection.scrollIntoView({ behavior: "smooth" });
+});
+
+document
+  .querySelector("#TransportasiRamahLingkungan")
+  .addEventListener("click", () => {
+    const tentangKamiSection = document.getElementById("MotorListrik");
+    tentangKamiSection.scrollIntoView({ behavior: "smooth" });
   });
-
-  //menambahkan fitur auto scroll
-  document.querySelector("#EnergiTerbarukan").addEventListener("click", () => {
-    const tentangKamiSection = document.getElementById("PanelSurya");
-    tentangKamiSection.scrollIntoView({ behavior: 'smooth' });
-});
-
-document.querySelector("#TransportasiRamahLingkungan").addEventListener("click", () => {
-  const tentangKamiSection = document.getElementById("MotorListrik");
-  tentangKamiSection.scrollIntoView({ behavior: 'smooth' });
-});
 document.querySelector("#PengelolaanLimbah").addEventListener("click", () => {
   const tentangKamiSection = document.getElementById("DaurUlang");
-  tentangKamiSection.scrollIntoView({ behavior: 'smooth' });
+  tentangKamiSection.scrollIntoView({ behavior: "smooth" });
 });
-document.querySelector("#PertanianBerkelanjutan").addEventListener("click", () => {
-  const tentangKamiSection = document.getElementById("PertanianOrganik");
-  tentangKamiSection.scrollIntoView({ behavior: 'smooth' });
-});
+document
+  .querySelector("#PertanianBerkelanjutan")
+  .addEventListener("click", () => {
+    const tentangKamiSection = document.getElementById("PertanianOrganik");
+    tentangKamiSection.scrollIntoView({ behavior: "smooth" });
+  });
+document
+  .querySelector("#TentangKami")
+  .addEventListener("click", () => {
+    const tentangKamiSection = document.getElementById("TentangKamiSection");
+    tentangKamiSection.scrollIntoView({ behavior: "smooth" });
+  });
